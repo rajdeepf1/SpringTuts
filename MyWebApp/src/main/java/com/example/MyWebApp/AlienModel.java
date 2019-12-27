eb7c5704442dd87aeb322f4a2aaeed59426cee9e
rajdeepf1 <rajdeepf1@gmail.com>
2019-12-27 11:43:23 +0530

    passing whole model object to ur jsp page

---
+++ MyWebApp/src/main/java/com/example/MyWebApp/AlienModel.java
@@ -0,0 +1,37 @@
+package com.example.MyWebApp;
+
+public class AlienModel {
+	
+	private int aID;
+	
+	private String alienName;
+
+	private String alienLang;
+
+	public int getaID() {
+		return aID;
+	}
+
+	public void setaID(int aID) {
+		this.aID = aID;
+	}
+
+	public String getAlienName() {
+		return alienName;
+	}
+
+	public void setAlienName(String alienName) {
+		this.alienName = alienName;
+	}
+
+	public String getAlienLang() {
+		return alienLang;
+	}
+
+	public void setAlienLang(String alienLang) {
+		this.alienLang = alienLang;
+	}
+	
+	
+
+}
