950a37eb046421b3ca8c4d380bd5e3b4cc7e68d8
rajdeepf1 <rajdeepf1@gmail.com>
2019-12-27 17:29:06 +0530

    JPA

--- bootjpa/src/main/java/com/example/bootjpa/Controller/Controller.java
+++ bootjpa/src/main/java/com/example/bootjpa/Controller/Controller.java
@@ -1,8 +1,10 @@
 package com.example.bootjpa.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.bootjpa.dao.AlienRepo;
@@ -36,7 +38,30 @@ public class Controller {
 		
 		modelAndView.addObject("obj",alien);
 		
+		System.out.println(repo.findByTech("Spring Boot"));
+		
+		
 		return modelAndView;
 	}
+	
+	@RequestMapping("/getAliens")
+	@ResponseBody
+	public String getAlien() {
+		
+		
+		
+		
+		return repo.findAll().toString();
+	}
+	
+	
+	@RequestMapping("/getAlienBy/{aid}")
+	@ResponseBody
+	public String getAlienBy(@PathVariable("aid") int aid) {
+	
+		
+		return repo.findById(aid).toString();
+	}
+	
 	
 }
--- bootjpa/src/main/java/com/example/bootjpa/dao/AlienRepo.java
+++ bootjpa/src/main/java/com/example/bootjpa/dao/AlienRepo.java
@@ -1,9 +1,16 @@
 package com.example.bootjpa.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.bootjpa.model.AlienModel;
 
+
 public interface AlienRepo extends CrudRepository<AlienModel, Integer>{
 
+	List<AlienModel> findByTech(String tech);
+	
+	List<AlienModel> findByAidGreaterThan(int aid);
+	
 }
