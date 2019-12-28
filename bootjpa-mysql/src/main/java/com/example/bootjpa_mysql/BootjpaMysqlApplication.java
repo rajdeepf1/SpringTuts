91b2ce7cdcc7295b57db6c1d394c3b5732ad7240
rajdeepf1 <rajdeepf1@gmail.com>
2019-12-28 13:23:33 +0530

    bootjpa-mysql project is an example for external database such as mysql and create post api and get api

---
+++ bootjpa-mysql/src/main/java/com/example/bootjpa_mysql/BootjpaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootjpa_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootjpaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootjpaMysqlApplication.class, args);
+	}
+
+}
